0d337532d76a235741b6180cb5092b038d4cef18
madriesen <martijn.driesen5@hotmail.com>
2020-04-14 14:00:44 +0200

    remove composer and package lock

--- config/database.php
+++ config/database.php
@@ -43,13 +43,6 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'sqlite_testing' => [
-            'driver' => 'sqlite',
-            'url' => env('TEST_DATABASE_URL'),
-            'database' => ':memory:',
-            'prefix' => ''
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
