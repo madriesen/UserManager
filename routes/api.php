5dccfeff60e5ed948c4d7bfe58495df90e7809af
madriesen <martijn.driesen5@hotmail.com>
2020-04-14 14:20:53 +0200

    update captials in route file

--- routes/api.php
+++ routes/api.php
@@ -17,26 +17,26 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::group(['namespace' => 'auth\registration\memberrequest', 'middleware' => 'Api', 'prefix' => 'registration/memberrequest'], function () {
+Route::group(['namespace' => 'Auth\Registration\Memberrequest', 'middleware' => 'Api', 'prefix' => 'registration/memberrequest'], function () {
     Route::post('/create', 'MemberRequestController')->name('memberRequest');
     Route::post('/approve', ['uses' => 'MemberRequestController@response', 'response' => 'approve'])->name('approveMemberRequest');
     Route::post('/refuse', ['uses' => 'MemberRequestController@response', 'response' => 'refuse'])->name('refuseMemberRequest');
     Route::get('/all', 'MemberRequestController@getAll')->name('getAllMemberRequests');
 });
 
-Route::group(['namespace' => 'auth\registration\invite', 'middleware' => 'Api', 'prefix' => 'registration/invite'], function () {
+Route::group(['namespace' => 'Auth\Registration\Invite', 'middleware' => 'Api', 'prefix' => 'registration/invite'], function () {
     Route::post('/create', 'InviteController')->name('invite');
     Route::post('/accept', ['uses' => 'InviteController@response', 'response' => 'accept'])->name('acceptInvite');
     Route::post('/decline', ['uses' => 'InviteController@response', 'response' => 'decline'])->name('declineInvite');
     Route::get('/all', 'InviteController@getAll')->name('getAllInvites');
 });
 
-Route::group(['namespace' => 'auth\account', 'middleware' => 'Api', 'prefix' => 'account'], function () {
+Route::group(['namespace' => 'Auth\Account', 'middleware' => 'Api', 'prefix' => 'account'], function () {
     Route::post('/create', 'AccountController')->name('account');
     Route::get('/all', 'AccountController@getAll')->name('getAllAccounts');
 });
 
-Route::group(['namespace' => 'auth\profile', 'middleware' => 'Api', 'prefix' => 'profile'], function () {
+Route::group(['namespace' => 'Auth\Profile', 'middleware' => 'Api', 'prefix' => 'profile'], function () {
     Route::post('/create', 'ProfileController')->name('profile');
     Route::post('/update', 'ProfileController@update')->name('updateProfile');
 });
