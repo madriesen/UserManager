1422d7ad7850efbdef8f1a0bfc57c1b52195e0fb
madriesen <martijn.driesen5@hotmail.com>
2020-04-26 17:32:03 +0200

    refactor member_request_test

---
+++ tests/Unit/Http/Controllers/Auth/Registration/MemberRequest/MemberRequestRoutesTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers\Auth\Registration\MemberRequest;
+
+use Tests\TestCase;
+
+class MemberRequestRoutesTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+    /** @test */
+    public function a_test_without_arguments_passes(){
+        return true;
+    }
+}
